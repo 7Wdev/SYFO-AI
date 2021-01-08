9d67fd8e609899a016dffb2eb8d12ac8612dfefe
Ade Issawe <45117834+7Wdev@users.noreply.github.com>
2021-01-08 14:46:04 +0200

    Add files via upload

---
+++ SYFO/SYFO-AI - ENC.js
@@ -0,0 +1,198 @@
+/*jslint plusplus: true, evil: true */
+/*global console, alert, prompt*/
+
+/* By 7Wdev */
+/* var string = "hi bye love chooclate",
+    result = string.replace(/l/ig, " ");
+
+console.log(result); */
+/* for education stuff*/
+
+/*                              var
+                    tName="-1x^2   +2.9x",
+                         syfo = console;
+abc = tName.replace(/x/g, "").replace("^2", "").replace(/\s/g, "");
+       $re = /-?\d+(?:\.\d+)?/mgi;$str = abc.toString();
+                    var $aa = $str.match($re);
+*/
+
+/* This code is free to use I coded it for educational and experemintal uses only!
+ * Name: SYFO-AI
+ * Type: Bot
+ * Author: 7Wdev
+ * Version: 0.2.0-alpha
+ * All copyrights are reserved!
+ * Feel free to edit the code, feel free to contribute...
+ * Desctription: Basic AI that can solve squared(quadric) equations...
+ * Known issues: this bot does not understand Roots, Fractions, the Quadric Formula(law), Can not arrange the equation etc...
+ * This AI uses Trinom(law) to solve the equations!
+ * Obfucation Level = simple 
+ */
+
+//----------------------------------------------------------------------------------------------------------------------------//
+
+                                                      var _0x216f = 
+
+
+
+[    '\x0a-\x20N1\x20*\x20N2\x20=\x20C','Please\x20be\x20sure\x20that\x20the\x20equation\x20is\x20arranged\x20correctly,\x0aOr\x20check\x20if\x20It\x20is\x20a\x20squared\x20equation...','247675YUXgHV',
+   'Hi,\x20I\x20am\x20good\x20at\x20solving\x20squared\x20equations!','277841ctxrgW','\x0a\x0a\x20We\x20divide\x20all\x20the\x20equation\x20by\x20A!','1782WUzArj','\x0aC:\x20',
+ '8587galLFm','\x0aA:\x20','18TlnpVU','\x0a-\x20N1\x20+\x20N1\x20=\x20B',')\x20=\x200','59DHeUmz','\x0a-\x20(','length','replace','646168fnrMhh','\x20-\x20U)\x20*\x20(',
+'sqrt','\x20*\x20','Please\x20put\x20only\x20one\x20variable!\x0a(put\x20an\x20upper-case\x20or\x20a\x20lower-case\x20\x27x\x27\x20only\x20in\x20your\x20equation)',
+  '\x0aB:\x20','\x0aAfter\x20that\x20lets\x20check\x20if\x20there\x20is\x20solution\x0aFor\x20this\x20equation\x20or\x20there\x20isn\x27t!',
+     '\x0a\x0aNow\x20we\x20know\x20the\x20N1\x20and\x20N2:',
+      'indexOf',
+      'toString',
+      'I\x20can\x20not\x20solve\x20this\x20equation,\x20sorry\x20dude\x20;(','\x20-\x20',
+       '\x0a\x0aMiddle\x20point\x20=\x20','X1\x20=\x20','\x0a\x0aTo\x20find\x20the\x20N1\x20and\x20N2\x20we\x20should\x20get\x20the\x20middle\x20point\x20of\x20B:',
+       'NOTE:\x20Don\x27t\x20be\x20lazy,\x20arrange\x20the\x20equation\x20or\x0aI\x20may\x20solve\x20the\x20equation\x20wrongly!\x0a','slice','However\x20this\x20equation\x20doesn\x27t\x20seem\x20familiar\x20to\x20me...',
+                                                                                                                                                                                                           '\x0aX1\x20=\x20',
+                                                                                                                                                                                                                'concat',
+                                                                                                                                                                                '\x20-U^2\x20=\x20','match',')\x20*\x20(x\x20+\x20(',
+                        '\x0a-\x20','First\x20of\x20all\x20as\x20we\x20learnt\x20in\x20the\x20f*cking\x20school,\x0aYou\x20should\x20extract\x20the\x20values\x20of\x20each\x20one\x0aOf\x20the\x20A\x20+\x20B\x20+\x20C\x20params!',
+  '232938dCrRcV','158874OVjrVx','\x20+\x20U)\x20=\x20','\x0a-\x20B\x20/\x202\x20->\x20','\x20=\x200]','\x0a√(',')\x0aReturns\x20a\x20negative\x20number\x20so\x20that\x20means,\x20we\x20can\x20not\x20solve\x20this\x20equation...',
+  '\x0a-\x20[','^2\x20-\x204\x20*\x20',')\x0aReturns\x20a\x20positive\x20number\x20so\x20that\x20means,\x20we\x20can\x20solve\x20this\x20equation...',',\x20X2\x20=\x20','\x0a-\x20U\x20=\x20√',
+'\x20|\x20N1\x20------\x20Middle\x20------\x20N2\x20|\x20U\x20is\x20the\x20distance\x20between\x20N1\x20and\x20the\x20Middle,\x20N2\x20and\x20the\x20Middle:'];
+
+
+
+//----------------------------------------------------------------------------------------------------------------------------//
+
+
+
+var _0x8bdc=function(_0x2d1f83,_0x23eb64){_0x2d1f83=                                                                                   _0x2d1f83-0xfb;var _0x216f9e=_0x216f[_0x2d1f83];return _0x216f9e;};
+           var _0x566267=function(_0x2502ab,_0x41341e)                                                                               {return _0x8bdc(_0x2502ab-0x33d,_0x41341e);};
+            (function(_0x4cba64,_0x35b9ea){var _0xfcc071=function(                                           _0x3c37df,_0x3f6f00){return _0x8bdc(_0x3c37df- -0x2b0,_0x3f6f00);};
+                 while(!![]){try{var _0x364310=parseInt(_0xfcc071(-0x1a9,-0x19c))+-parseInt(_0xfcc071(-0x1a8,-0x1bd))+-parseInt(_0xfcc071(-0x198,-0x194))+-parseInt(
+                                                            _0xfcc071(-0x18f,-0x186))*-parseInt(_0xfcc071(-0x196,//
+                                                            -0x181))+-parseInt(_0xfcc071(-0x19a,-0x1ab))+parseInt(//
+                                                            _0xfcc071(-0x192,-0x17b))*-parseInt(_0xfcc071(-0x194,//$
+                                                            -0x1a9))+parseInt(_0xfcc071(-0x18b,-0x186));if(_0x364310
+                                                            ===_0x35b9ea)break;else _0x4cba64['push'](_0x4cba64[///
+                                                            'shift']());}catch(_0x5b55bf){_0x4cba64['push'//$$$$$//
+                                                            ](_0x4cba64['shift']());}}}(_0x216f,0x23788),SEND(////
+                                                            _0x566267(0x43a,0x44a)));//$$$$$$$$$$$$$$$$$$$$$$$$$//
+                                                            //////////////////////////////////////////////////////
+                                                            //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$//
+                                                            //////////////////////////////////////////////////////
+                                                             //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$//
+                                                             //////////////////////////////////////////////////
+                                           //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$//
+
+                                           
+
+//----------------------------------------------------------------------------------------------------------------------------//
+
+
+
+var I = "x^2 -36";
+const SYFO = console;
+var A, B, C;
+
+
+
+//----------------------------------------------------------------------------------------------------------------------------//
+
+
+
+function SEND(_0x1c9bee){return console['log'](_0x1c9bee);}function isSolvable(){if(parseFloat(B)*parseFloat(B)-0x4*(parseFloat(A)*parseFloat(C))>=0x0)return !![];if
+(parseFloat(B)*parseFloat(B)-0x4*(parseFloat(A)*parseFloat(C))<0x0)return ![];}function findABC(_0x105d88){var _0x270761=function(_0x15a621,_0x4daeec){return _0x566267(
+_0x4daeec- -0x212,_0x15a621);//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$//
+},_0x1e71e5='x',_0xa6800a='X',/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+_0x11f597='^2',
+_0x56ea22,//$$//
+_0x2548f5,//////
+_0x1f2716,//$$//
+_0x22e56e;if(_0x105d88[_0x270761(0x25b,0x258)](_0x1e71e5[_0x270761(0x233,0x22c)](_0x11f597))!=-0x1||_0x105d88[_0x270761(0x23d,0x258)]
+(_0xa6800a['concat'](_0x11f597))!=-0x1){_0x56ea22=_0x105d88[_0x270761(0x267,0x258)](_0x1e71e5[_0x270761(0x21b,0x22c)](_0x11f597)),_0x2548f5=
+_0x105d88['indexOf'](_0xa6800a[_0x270761(0x23d,0x22c)](_0x11f597));if(_0x56ea22!=-0x1){A=_0x105d88[_0x270761(0x226,0x229)](0x0,//////
+_0x56ea22)//$/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+[_0x270761(0x260,
+0x24f)](/\s/g,'')
+[_0x270761(0x249,
+0x24f)](/\*/g,'')
+['replace']/////
+(/[+]/g,'');//$//
+if(A==''||A=='+')
+A='1';if(A=='-')
+A='-1';}else{///
+if(_0x2548f5!=-0x1){
+A=_0x105d88[_0x270761
+(0x215,0x229)](0x0,_0x2548f5)[_0x270761(
+//////////////////////////////////////
+
+
+
+//----------------------------------------------------------------------------------------------------------------------------//
+
+
+
+                                           //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$//
+                                  ///////////////////////////////////////////////////////////////////////
+                        0x25b,0x24f)](/\s/g,'')[_0x270761(0x267,0x24f)](/\*/g,'')[_0x270761(0x254,0x24f)](/[+]/g,'');
+               if(A==''||A=='+')A='1';if(A=='-')A='-1';}}}else{}if(_0x105d88[_0x270761(0x247,0x258)](_0x1e71e5,_0x56ea22+0x4)
+        !=-0x1||_0x105d88[_0x270761(0x24a,0x258)](_0xa6800a,_0x2548f5+0x4)!=-0x1){_0x1f2716=_0x105d88[_0x270761(0x255,0x258)](_0x1e71e5,
+_0x56ea22+0x4),_0x22e56e=_0x105d88[                                                                         _0x270761(0x25d,0x258)](_0xa6800a,_0x2548f5
++0x4);if(_0x1f2716!=-0x1){B=                                                                                         _0x105d88[_0x270761(0x225,0x229)](
+_0x56ea22+0x4,_0x1f2716)                                                                                             [_0x270761(0x266,0x24f)](/\s/g,'')
+[_0x270761(0x25a, 0x24f)]                                                                                             (/\*/g,'')['replace'](/[+]/g,'');
+if(B==''||B=='+')B='1';                                                                                                      if(B=='-')B='-1';}else{if(
+_0x22e56e!=-0x1){/*$$*/                                                                                                      B=_0x105d88[_0x270761(0x236
+,0x229)](_0x2548f5+0x4,                                                                                                      _0x22e56e)[_0x270761(0x244,
+0x24f)](/\s/g,'')/*$$*/                                                                                                       [_0x270761(0x247,0x24f)]//
+(/\*/g,'')[/*$$$$$$$$*/                                                                                                       _0x270761(0x23a,0x24f)]///
+(/[+]/g,'');if(B==''||B                                                                                                       =='+')B='1';if(B=='-')B=//
+'-1';}}}else B='0';if(B                                                                                                       !='0'){if(_0x105d88[//////
+'length']<=_0x1f2716)C=                                                                                                       '0';else{if(_0x1f2716/////
+!=-0x1){$=/*$$$$$$$$$*/                                                                                                       _0x105d88[_0x270761(0x246,
+0x24e)],C=/*$$$$$$$$$*/                                                                                                       _0x105d88[_0x270761(0x23b,
+0x229)](_0x1f2716+ 0x1,                                                                                                       $)[_0x270761(0x268,0x24f)]
+(/\s/g,'')[/*$$$$$$$$*/                                                                                                       _0x270761(0x238,0x24f)]///
+(/\*/g,'')[/*$$$$$$$$*/                                                                                                       _0x270761(0x23b,0x24f)]///
+(/[+]/g,'');if(C==''||C                                                                                                       =='+')C='0';if(C=='-')C='0'
+;}else{if(_0x22e56e/**/                                                                                                       !=-0x1){$=_0x105d88[//////
+_0x270761(0x245,0x24e)],C=_0x105d88[                                                                        _0x270761(0x23d,0x229)](_0x22e56e+0x1,$)[///
+    _0x270761(0x250,0x24f)](/\s/g,'')[_0x270761(0x25a,0x24f)](/\*/g,'')[_0x270761(0x23f,0x24f)](/[+]/g,'');if(C==''||C=='+')C='0';if(C=='-')C='0';//
+           }}}}else{if(_0x105d88['length']<=_0x56ea22+0x3||_0x105d88['length']<=_0x2548f5+0x3)C='0';else{if(_0x56ea22!=-0x1){$2=_0x105d88[////
+                    _0x270761(0x258,0x24e)],C=_0x105d88['slice'](_0x56ea22+0x3,$2)['replace'](/\s/g,'')[_0x270761(0x24e,////
+                                    0x24f)](/\*/g,'')['replace'](/[+]/g,'');if(C==''||C=='+')C='0';if(////
+
+
+                                     
+//----------------------------------------------------------------------------------------------------------------------------//
+
+/*
+ *
+ *
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+C=='-')C='0';}else{if(_0x2548f5!=-0x1){$2=_0x105d88['length'],C=_0x105d88[_0x270761(0x214,0x229)](_0x2548f5+0x3,$2)['replace'](/\s/g,'')['replace'](/\*/g,'')['replace'](/[+]/g,'');
+if(C==''||C=='+')C='0';
+if(C=='-')C='0';}}}}}
+if(I[_0x566267(0x46a,0x45a)]('/')!=-0x1||I['indexOf']('√')!=-0x1||I['indexOf']('*')!=-0x1)SEND(_0x566267(0x46c,0x466));else{
+if(I[_0x566267(0x46a,0x472)]('x')==-0x1&&I[_0x566267(0x46a,0x459)]('X')==-0x1)SEND(_0x566267(0x46c,0x45b));else{
+if(I['indexOf']('x')!=-0x1&&I[_0x566267(0x46a,0x467)]('X')!=-0x1)SEND(_0x566267(0x466,0x465));else{findABC(I);
+if(I[_0x566267(0x46a,0x472)]('x')!=-0x1||I[_0x566267(0x46a,0x46a)]('X')!=-0x1){
+if(A[_0x566267(0x440, 0x43d)](/^-?(\d+\.?\d*)$|(\d*\.?\d+)$/)!==null&&B[_0x566267(0x440,0x444)](/^-?(\d+\.?\d*)$|(\d*\.?\d+)$/)!==null&&C[_0x566267(0x440,0x426)](/^-?(\d+\.?\d*)$|(\d*\.?\d+)$/)!==null){$abcmsg=_0x566267(0x443,0x44e)+_0x566267(0x45a,0x448)+A+_0x566267(0x467,0x463)+B+_0x566267(0x458,0x45c)+C;
+if(I[_0x566267(0x46a,0x453)]('x')!=-0x1&&I[_0x566267(0x46a,0x469)]('X')==-0x1)SEND($abcmsg);
+if(I['indexOf']('x')==-0x1&&I[_0x566267(0x46a,0x472)]('X')!=-0x1)SEND($abcmsg);
+YES=!![],NO=![],SOLVABLE=new Boolean(isSolvable()),Trinom();}else{
+if(I[_0x566267(0x46a,0x461)]('x')!=-0x1&&I['indexOf']('X')!=-0x1){}else SEND(_0x566267(0x454,0x446)),SEND(_0x566267(0x43c,0x43b)), setTimeout(function(){var _0x23b3ee=function(_0x1d3f8b,_0x5f3ee2){return _0x566267(_0x5f3ee2-0xf3,_0x1d3f8b);};SEND(_0x23b3ee(0x535,0x545));},0x12c);}}}}}function Trinom(){var _0x225459=function(_0x5c2144,_0x3e1653){return _0x566267(_0x5c2144-0x14a,_0x3e1653);};SEND(_0x225459(0x5b2,0x5a6));
+if(SOLVABLE==YES){
+if(B!='0'||B!=0x0){SEND(_0x225459(0x593,0x595)+B+_0x225459(0x596,0x5a2)+A+_0x225459(0x5af,0x5c3)+C+_0x225459(0x597,0x58f)),A=parseFloat(A),B=parseFloat(B),C=parseFloat(C),B=B/A,C=C/A,A=A/A;let _0x82cd36=parseFloat(B)/0x2;SEND('\x0aLets\x20solve\x20it\x20via\x20Trinom:'+_0x225459(0x595,0x59a)+I+_0x225459(0x592,0x5a8)+_0x225459(0x5a0,0x5ab)+_0x225459(0x5a6,0x590)+_0x225459(0x59b,0x584)+'\x0a-\x20(x\x20+\x20N1)\x20*\x20(x\x20+\x20N2)'+_0x225459(0x583,0x572)+_0x225459(0x591,0x57f)+_0x82cd36[_0x225459(0x5b5,0x5c7)]()+_0x225459(0x5b8,0x5cd)+_0x82cd36[_0x225459(0x5b5,0x59f)]()+_0x225459(0x59a,0x5ab)+_0x225459(0x5a9,0x5ae)+_0x82cd36[_0x225459(0x5b5,0x5c6)]()+_0x225459(0x5ad,0x5a1)+_0x82cd36[_0x225459(0x5b5,0x5c0)]()+_0x225459(0x590,0x58f)+C[_0x225459(0x5b5,0x5cd)]()+_0x225459(0x58c,0x589)+(_0x82cd36*_0x82cd36)[_0x225459(0x5b5,0x5bd)]()+_0x225459(0x589,0x5a4)+C[_0x225459(0x5b5,0x5b0)]()+'\x0a-\x20-U^2\x20=\x20'+(C+-0x1*_0x82cd36*_0x82cd36)[_0x225459(0x5b5,0x5bb)]()+_0x225459(0x599,0x585)+(-0x1*(C+-0x1*_0x82cd36*_0x82cd36))['toString']()+_0x225459(0x5b3,0x59f)),setTimeout(function(){var _0x32d3cf=function(_0x33ccaf,_0xbe56cb){return _0x225459(_0x33ccaf- -0x2dd,_0xbe56cb);};SEND('-\x20(x\x20+\x20('+_0x82cd36['toString']()+_0x32d3cf(0x2da,0x2de)+'√'+(-0x1*(C+-0x1*_0x82cd36*_0x82cd36))[_0x32d3cf(0x2d8,0x2d9)]()+_0x32d3cf(0x2ae,0x2c1)+_0x82cd36[_0x32d3cf(0x2d8,0x2bf)]()+'\x20+\x20'+'√'+(-0x1*(C+-0x1*_0x82cd36*_0x82cd36))[_0x32d3cf(0x2d8,0x2ed)]()+_0x32d3cf(0x2ca,0x2be)+_0x32d3cf(0x2aa,0x2ab)+(-0x1*(_0x82cd36-Math[_0x32d3cf(0x2d1,0x2be)](-0x1*(C+-0x1*_0x82cd36*_0x82cd36))))[_0x32d3cf(0x2d8,0x2f2)]()+_0x32d3cf(0x2bb,0x2c9)+-0x1*(_0x82cd36+Math[_0x32d3cf(0x2d1,0x2bc)](-0x1*(C+-0x1*_0x82cd36*_0x82cd36))));},0x190);}else C=parseFloat(C),A=parseFloat(A),X1=Math['sqrt'](-0x1*C/A),X2=-0x1*X1,SEND(_0x225459(0x582,0x56d)+X1[_0x225459(0x5b5,0x5bd)]()+',\x20X2\x20=\x20'+X2[_0x225459(0x5b5,0x5cd)]());}
+if(SOLVABLE==NO)SEND(_0x225459(0x593, 0x579)+B+_0x225459(0x596,0x57a)+A+_0x225459(0x5af,0x5c5)+C+_0x225459(0x594,0x57d));}
+
+//----------------------------------------------------------------------------------------------------------------------------//
---
+++ SYFO/SYFO-AI.js
@@ -0,0 +1,239 @@
+/*jslint plusplus: true, evil: true */
+/*global console, alert, prompt*/
+
+/* By 7Wdev */
+/* var string = "hi bye love chooclate",
+    result = string.replace(/l/ig, " ");
+
+console.log(result); */
+/* for education stuff*/
+
+/*                              var
+                    tName="-1x^2   +2.9x",
+                         syfo = console;
+abc = tName.replace(/x/g, "").replace("^2", "").replace(/\s/g, "");
+       $re = /-?\d+(?:\.\d+)?/mgi;$str = abc.toString();
+                    var $aa = $str.match($re);
+*/
+
+/* This code is free to use I coded it for educational and experemintal uses only!
+ * Name: SYFO-AI
+ * Type: Bot
+ * Author: 7Wdev
+ * Version: 0.2.0-alpha
+ * All copyrights are reserved!
+ * Feel free to edit the code, feel free to contribute...
+ * Desctription: Basic AI that can solve squared(quadric) equations...
+ * Known issues: this bot does not understand Roots, Fractions, the Quadric Formula(law), Can not arrange the equation etc...
+ * This AI uses Trinom(law) to solve the equations!
+ * Obfucation Level = null (not obfuscated)
+ */
+
+//----------------------------------------------------------------------------------------------------------------------------//
+
+SEND("NOTE: Don't be lazy, arrange the equation or\nI may solve the equation wrongly!\n");
+const I = "2x^2 +4x -4";
+const SYFO = console;
+var A,
+    B,
+    C;
+
+function SEND(string1)
+  {
+      return console.log(string1);
+  }
+
+function isSolvable()
+  {
+      if((parseFloat(B) * parseFloat(B)) - 4 * (parseFloat(A) * parseFloat(C)) >= 0) return true;
+      if((parseFloat(B) * parseFloat(B)) - 4 * (parseFloat(A) * parseFloat(C)) < 0) return false;
+  }
+
+function findABC(INPUT)
+  {
+      var XSTR = "x",
+          XSTRB = "X",
+          XP2 = "^2",
+          $i,
+          $ib,
+          $ii,
+          $iib;
+
+      if(INPUT.indexOf(XSTR.concat(XP2)) != -1 || INPUT.indexOf(XSTRB.concat(XP2)) != -1)
+       {
+           $i = INPUT.indexOf(XSTR.concat(XP2));
+           $ib = INPUT.indexOf(XSTRB.concat(XP2));
+           if($i != -1)
+            {
+                A = INPUT.slice(0, $i).replace(/\s/g, "").replace(/\*/g, "").replace(/[+]/g, "");
+                if(A == "" || A == "+") A = "1";
+                if(A == "-") A = "-1";
+            } else if($ib != -1)
+                {
+                    A = INPUT.slice(0, $ib).replace(/\s/g, "").replace(/\*/g, "").replace(/[+]/g, "");
+                    if(A == "" || A == "+") A = "1";
+                    if(A == "-") A = "-1";
+                }
+       } else
+           {
+               //Overiding The Msg;
+           }
+
+      if(INPUT.indexOf(XSTR, $i + 4) != -1 || INPUT.indexOf(XSTRB, $ib + 4) != -1)
+       {
+           $ii = INPUT.indexOf(XSTR, $i + 4);
+           $iib = INPUT.indexOf(XSTRB, $ib + 4);
+           if($ii != -1)
+            {
+                B = INPUT.slice($i + 4, $ii).replace(/\s/g, "").replace(/\*/g, "").replace(/[+]/g, "");
+                if(B == "" || B == "+") B = "1";
+                if(B == "-") B = "-1";
+            } else if($iib != -1)
+                {
+                    B = INPUT.slice($ib + 4, $iib).replace(/\s/g, "").replace(/\*/g, "").replace(/[+]/g, "");
+                    if(B == "" || B == "+") B = "1";
+                    if(B == "-") B = "-1";
+                }
+       } else 
+           {
+               B = "0";
+           }
+
+      if(B != "0")
+       {
+           if(INPUT.length <= $ii)
+            {
+                C = "0";
+            } else if($ii != -1)
+                {
+                    $ = INPUT.length;
+                    C = INPUT.slice($ii + 1, $).replace(/\s/g, "").replace(/\*/g, "").replace(/[+]/g, "");
+                    if(C == "" || C == "+") C = "0";
+                    if(C == "-") C = "0";
+                } else if($iib != -1)
+                    {
+                        $ = INPUT.length;
+                        C = INPUT.slice($iib + 1, $).replace(/\s/g, "").replace(/\*/g, "").replace(/[+]/g, "");
+                        if(C == "" || C == "+") C = "0";
+                        if(C == "-") C = "0";
+                    }
+       } else
+           {
+               if(INPUT.length <= $i + 3 || INPUT.length <= $ib + 3)
+                {
+                    C = "0";
+                } else if($i != -1)
+                    {
+                        $2 = INPUT.length;
+                        C = INPUT.slice($i + 3, $2).replace(/\s/g, "").replace(/\*/g, "").replace(/[+]/g, "");
+                        if(C == "" || C == "+") C = "0";
+                        if(C == "-") C = "0";
+                    } else if($ib != -1)
+                        {
+                            $2 = INPUT.length;
+                            C = INPUT.slice($ib + 3, $2).replace(/\s/g, "").replace(/\*/g, "").replace(/[+]/g, "");
+                            if(C == "" || C == "+") C = "0";
+                            if(C == "-") C = "0";
+                        }
+
+           }
+  }
+
+//----------------------------------------------------------------------------------------------------------------------------//
+
+if(I.indexOf("/") != -1 || I.indexOf("√") != -1 || I.indexOf("*") != -1)
+ {
+     SEND("I can not solve this equation, sorry dude ;(");
+ } else if(I.indexOf("x") == -1 && I.indexOf("X") == -1)
+     {
+         SEND("I can not solve this equation, sorry dude ;(");
+     } else if(I.indexOf("x") != -1 && I.indexOf("X") != -1)
+         {
+             SEND("Please put only one variable!\n(put an upper-case or a lower-case 'x' only in your equation)");
+         } else
+             {
+                 findABC(I);
+                 if(I.indexOf("x") != -1 || I.indexOf("X") != -1)
+                  {
+                      if(A.match(/^-?(\d+\.?\d*)$|(\d*\.?\d+)$/) !== null && B.match(/^-?(\d+\.?\d*)$|(\d*\.?\d+)$/) !== null && C.match(/^-?(\d+\.?\d*)$|(\d*\.?\d+)$/) !== null)
+                       {
+                           $abcmsg = "First of all as we learnt in the f*cking school,\nYou should extract the values of each one\nOf the A + B + C params!" + "\nA: " + A + "\nB: " + B + "\nC: " + C;
+                           if(I.indexOf("x") != -1 && I.indexOf("X") == -1) SEND($abcmsg);
+                           if(I.indexOf("x") == -1 && I.indexOf("X") != -1) SEND($abcmsg);
+                           YES = true,
+                           NO = false,
+                           SOLVABLE = new Boolean(isSolvable());
+                           Trinom();
+                       } else
+                           {
+                               if(I.indexOf("x") != -1 && I.indexOf("X") != -1)
+                                {
+                                    //Nothing...
+                                } else
+                                    {
+                                        SEND("Hi, I am good at solving squared equations!");
+                                        SEND("However this equation doesn't seem familiar to me...");
+                                        setTimeout(function(){SEND("Please be sure that the equation is arranged correctly,\nOr check if It is a squared equation...");}, 300);
+                                    }
+                           }
+                  }
+             }
+
+//----------------------------------------------------------------------------------------------------------------------------//
+
+function Trinom()
+  {
+      SEND("\nAfter that lets check if there is solution\nFor this equation or there isn't!");
+      if(SOLVABLE == YES)
+       {
+           if(B != "0" || B != 0)
+            {
+                SEND("\n√(" + B + "^2 - 4 * " + A + " * " + C + ")\nReturns a positive number so that means, we can solve this equation...");
+                A = parseFloat(A);
+                B = parseFloat(B);
+                C = parseFloat(C);
+                B = B / A;
+                C = C / A;
+                A = A / A;
+                let middlepoint = parseFloat(B) / 2;
+                SEND("\nLets solve it via Trinom:" +
+                     "\n- [" + I + " = 0]" +
+                     "\n\n We divide all the equation by A!" +
+                     "\n- N1 + N1 = B" +
+                     "\n- N1 * N2 = C" +
+                     "\n- (x + N1) * (x + N2)" +
+                     "\n\nTo find the N1 and N2 we should get the middle point of B:" +
+                     "\n- B / 2 -> " + middlepoint.toString() +
+                     "\n\nMiddle point = " + middlepoint.toString() + " | N1 ------ Middle ------ N2 | U is the distance between N1 and the Middle, N2 and the Middle:" +
+                     "\n- (" + middlepoint.toString() + " - U) * (" + middlepoint.toString() + " + U) = " + C.toString() +
+                     "\n- " + (middlepoint * middlepoint).toString() + " -U^2 = " + C.toString() +
+                     "\n- -U^2 = " + (C + (-1 * middlepoint * middlepoint)).toString() +
+                     "\n- U = √" + (-1 * (C + (-1 * middlepoint * middlepoint))).toString() +
+                     "\n\nNow we know the N1 and N2:");
+
+                setTimeout(function(){SEND("- (x + (" + middlepoint.toString() + " - " + "√" + (-1 * (C + (-1 * middlepoint * middlepoint))).toString() + ") * (x + (" + middlepoint.toString() + " + " + "√" + (-1 * (C + (-1 * middlepoint * middlepoint))).toString() + ") = 0" + "\nX1 = " + (-1 * (middlepoint - Math.sqrt((-1 * (C + (-1 * middlepoint * middlepoint)))))).toString() + ", X2 = " + (-1 * (middlepoint + Math.sqrt((-1 * (C + (-1 * middlepoint * middlepoint)))))));}, 400);
+            } else
+                {
+                    C = parseFloat(C);
+                    A = parseFloat(A);
+                    X1 = Math.sqrt((-1 * C) / A);
+                    X2 = -1 * X1;
+                    SEND("X1 = " + X1.toString() + ", X2 = " + X2.toString());
+                }  
+       }
+      if(SOLVABLE == NO) SEND("\n√(" + B + "^2 - 4 * " + A + " * " + C + ")\nReturns a negative number so that means, we can not solve this equation...");
+   }
+
+//----------------------------------------------------------------------------------------------------------------------------//
+
+/*
+ * SOME TRASHY TEXTS
+ *
+ * 
+ * 
+ * 
+ * 
+ */
+
+var
+BOB = [];
